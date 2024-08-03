05d8b10d0bfc9e95b9f9025ca2705fd647f6fbc1
DESKTOP-QI756FO\User <nada_yk@naver.com>
2024-08-03 10:28:16 +0900

    24. 08. 03 집에서 커밋

--- Student복습/src/student/dto/StudentDTO.java
+++ Student복습/src/student/dto/StudentDTO.java
@@ -1,65 +1,97 @@
 package student.dto;
 
 public class StudentDTO {
-	private String studentNumber;
-	private String name;
-	private char gender;
-	private int html;
-	private int css;
-	private int js;
-	private int java;
 	
-	
-	public String getStudentNumber() {
-		return studentNumber;
-	}
-	public void setStudentNumber(String studentNumber) {
-		this.studentNumber = studentNumber;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public char getGender() {
-		return gender;
-	}
-	public void setGender(char gender) {
-		this.gender = gender;
-	}
-	public int getHtml() {
-		return html;
-	}
-	public void setHtml(int html) {
-		this.html = html;
-	}
-	public int getCss() {
-		return css;
-	}
-	public void setCss(int css) {
-		this.css = css;
-	}
-	public int getJs() {
-		return js;
-	}
-	public void setJs(int js) {
-		this.js = js;
-	}
-	public int getJava() {
-		return java;
-	}
-	public void setJava(int java) {
-		this.java = java;
-	}
-	
-	public String toString() {
+		private String studentNumber;
+		private String name;
+		private char gender;
+		private int html;
+		private int css;
+		private int js;
+		private int java;
 		
-		return String.format("%s / %g / %s\n"
-				+ "HTML : %d / CSS : %d / JS : %d / Java : %d",
-				studentNumber, gender, name, html, css, js, java);
+		public StudentDTO(String studentNumber, String name, char gender) {
+			super();
+			this.studentNumber = studentNumber;
+			this.name = name;
+			this.gender = gender;
+		}
+
+		public StudentDTO(int html, int css, int js, int java) {
+			super();
+			this.html = html;
+			this.css = css;
+			this.js = js;
+			this.java = java;
+		}
+
+		public String getStudentNumber() {
+			return studentNumber;
+		}
+
+		public void setStudentNumber(String studentNumber) {
+			this.studentNumber = studentNumber;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public char getGender() {
+			return gender;
+		}
+
+		public void setGender(char gender) {
+			this.gender = gender;
+		}
+
+		public int getHtml() {
+			return html;
+		}
+
+		public void setHtml(int html) {
+			this.html = html;
+		}
+
+		public int getCss() {
+			return css;
+		}
+
+		public void setCss(int css) {
+			this.css = css;
+		}
+
+		public int getJs() {
+			return js;
+		}
+
+		public void setJs(int js) {
+			this.js = js;
+		}
+
+		public int getJava() {
+			return java;
+		}
+
+		public void setJava(int java) {
+			this.java = java;
+		}
+
+		@Override
+		public String toString() {
+			return "StudentDTO [studentNumber=" + studentNumber + ", name=" + name + ", gender=" + gender + ", html="
+					+ html + ", css=" + css + ", js=" + js + ", java=" + java + "]";
+		}
 		
-	}
+		
+		
+		
+		
+	
 	
 	
 }
--- Student복습/src/student/run/StudentRun.java
+++ Student복습/src/student/run/StudentRun.java
@@ -5,10 +5,15 @@ import student.view.StudentView;
 public class StudentRun {
 	
 	public static void main(String[] args) {
+	
 		
 		StudentView view = new StudentView();
 		
 		
 	}
-
+	
+	
+	
+		
+	
 }
